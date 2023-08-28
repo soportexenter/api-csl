14a364b68b2dea9af5b44e578a6e7a0c75e6ed61
lhernandez0105 <126634329+lhernandez0105@users.noreply.github.com>
2023-08-28 14:58:55 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,89 +1,138 @@
-import fetch from 'node-fetch';
-import fs from 'fs';
 import axios from 'axios';
+import fs from 'fs';
+import cron from 'node-cron'; // Asegúrate de importar el módulo 'node-cron'
 
 const urlFetch = "https://api.dentalink.healthatom.com/api/v1/pacientes";
-const urlPost = "https://api.clientify.net/v1/contacts/test";
-const apiKey = "038727a74b865e6da82c6aa435f4f9e5a166a35d";
+const urlPost = "https://api.clientify.net/v1/contacts/";
 const apiKeyConsulta = "i1M88WwcvHZ1vUBGDnpDyXYDf2TFpbYuRjoeVh64.OXaPjS9OPDTIPZBaC8SzNQrIWnzCfrIGPhls05ub";
+const apiKey = "529f345a1219496efc6bc6664b76cf6aeebaea3c";
+
+const header = {
+  "Authorization": `Token ${apiKeyConsulta}`
+};
 
 const headers = {
   "Authorization": `Token ${apiKey}`
 };
-const header = {
-  "Authorization": `Token ${apiKeyConsulta}`
+
+const generateRandomEmail = (name, surname) => {
+  const randomString = Math.random().toString(36).substring(7);
+  const email = `clinicasantaluciana.emailRandom.${name.toLowerCase()}.${surname.toLowerCase()}.${randomString}@example.com`;
+  return email;
 };
 
 const capitalize = (str) => {
   return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
 };
 
-const sendDataToAPI = () => {
-  fetch(urlFetch, {
-    headers: header
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      const patients = data.data; // Obtener la lista de pacientes
+const getAllContacts = async () => {
+  let hasNextPage = true;
+  let cursor = null;
+  const allContacts = [];
 
-      fs.writeFile('datosaEnviar.json', JSON.stringify(data, null, 2), (err) => {
-        if (err) {
-          console.error('Error al guardar los datos:', err);
-        } else {
-          console.log('Datos guardados correctamente en datostest.json');
-          processAllPatients(patients); // Llamar a la función para procesar todos los pacientes
-        }
-      });
-    })
-    .catch((error) => {
+  while (hasNextPage) {
+    const url = cursor ? `${urlFetch}?cursor=${cursor}` : urlFetch;
+
+    try {
+      const response = await axios.get(url, { headers: header });
+      const data = response.data;
+
+      const contacts = data.data;
+      allContacts.push(...contacts);
+
+      if (data.links && data.links.next) {
+        const nextPageUrl = new URL(data.links.next);
+        cursor = nextPageUrl.searchParams.get("cursor");
+      } else {
+        hasNextPage = false;
+      }
+    } catch (error) {
       console.error('Error al obtener los datos desde la API:', error);
-    });
-};
+      break;
+    }
+  }
 
-const processAllPatients = (patients) => {
-  patients.forEach((patient) => {
-    processJsonData(patient); // Llamar a la función de procesamiento para cada paciente
+  // Sort contacts by fecha_nacimiento in descending order
+  return allContacts.sort((a, b) => {
+    const dateA = new Date(a.fecha_nacimiento);
+    const dateB = new Date(b.fecha_nacimiento);
+    return dateB - dateA;
   });
 };
 
+const sendFirstContactToAPI = async () => {
+  try {
+    const allContacts = await getAllContacts();
 
-const processJsonData = (patient) => { // Changed parameter name to clarify it represents a patient object
-  const { nombre, apellidos, email, telefono, direccion, ciudad, fecha_nacimiento } = patient;
+    if (allContacts.length > 0) {
+      const firstContact = allContacts[0];
 
-  const emailObj = {
-    type: 4,
-    email: email
-  };
+      const { nombre, apellidos, email, telefono, direccion, ciudad, fecha_nacimiento } = firstContact;
 
-  const telefonoObj = {
-    type: 1,
-    telefono: telefono
-  };
+      const emailObj = {
+        type: 4,
+        email: email //|| generateRandomEmail(nombre, apellidos)
+      };
 
-  const direccionObj = {
-    type: 1,
-    street: direccion,
-    city: ciudad
-  };
+      const telefonoObj = {
+        type: 1,
+        telefono: telefono
+      };
 
-  const selectedData = {
-    first_name: capitalize(nombre), // Fixed parameter name
-    last_name: capitalize(apellidos), // Fixed parameter name
-    emails: [emailObj],
-    phones: [telefonoObj],
-    addresses: [direccionObj],
-    birthday: fecha_nacimiento
-  };
+      const direccionObj = {
+        type: 1,
+        street: direccion,
+        city: ciudad
+      };
 
-  console.log('Datos a enviar:', selectedData);
+      const selectedData = {
+        first_name: capitalize(nombre), 
+        last_name: capitalize(apellidos), 
+        emails: [emailObj],
+        phones: [telefonoObj],
+        addresses: [direccionObj],
+        // birthday: fecha_nacimiento
+      };
 
-  axios.post(urlPost, selectedData, { headers })
-    .then(response => {
-      console.log('Respuesta del servidor:', response.data);
-    })
-    .catch(error => {
-      console.error('Error al realizar la solicitud POST:', error.message);
-    });
+      console.log('Datos a enviar:', selectedData);
+
+      axios.post(urlPost, selectedData, { headers })
+        .then(response => {
+          console.log('Respuesta del servidor:', response.data);
+        })
+        .catch(error => {
+          console.error('Error al realizar la solicitud POST:', error.message);
+        });
+
+      fs.writeFile('primer_contacto.json', JSON.stringify(firstContact, null, 2), (err) => {
+        if (err) {
+          console.error('Error al guardar el primer contacto:', err);
+        } else {
+          console.log('Primer contacto guardado correctamente en primer_contacto.json');
+        }
+      });
+    } else {
+      console.log('No hay contactos disponibles en el archivo.');
+    }
+  } catch (error) {
+    console.error('Error general:', error);
+  }
 };
 
-sendDataToAPI();
+//sendFirstContactToAPI();
+
+// Función que ejecuta el código
+const executeCode = async () => {
+    try {
+      await sendFirstContactToAPI();
+      console.log('Código ejecutado correctamente.');
+    } catch (error) {
+      console.error('Error al ejecutar el código:', error);
+    }
+  };
+  
+  // Cron para ejecutar el código cada 20 minutos
+  cron.schedule('* * * * *', () => {
+    console.log('Ejecutando el código cada 20 minutos.');
+    executeCode();
+  });
