bb4d3cf7fcf827acc9c3ee6e897da541b02ee925
lhernandez0105 <126634329+lhernandez0105@users.noreply.github.com>
2023-09-04 21:00:47 -0500

    Update app.js

--- app.js
+++ app.js
@@ -129,7 +129,7 @@ const sendFirstContactToAPI = async () => {
         owner_name: "Claudia Yanes",
         owner: "contactenos@clinicasantaluciana.com.co",
         tags: ["Dentalink","api-test"],
-        comment: "cedula" + rut
+        description: "cedula" + rut
         //birthday: fecha_nacimiento
       };
 
