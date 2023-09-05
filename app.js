6ea5eb858e9adadf7b6c45c94ffc7bb37f4de0f1
soportexenter <144149288+soportexenter@users.noreply.github.com>
2023-09-05 10:50:28 -0500

    moment app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import fs from 'fs';
 import cron from 'node-cron';
-import moment from 'moment';
+//import moment from 'moment';
 
 
 const urlFetch = "https://api.dentalink.healthatom.com/api/v1/pacientes";
@@ -124,7 +124,7 @@ const sendFirstContactToAPI = async () => {
         nit: rut
       };
 
-      const formattedBirthday = moment(fecha_nacimiento).format(); // Formatea la fecha de nacimiento
+      //const formattedBirthday = moment(fecha_nacimiento).format(); // Formatea la fecha de nacimiento
 
       
       const selectedData = {
